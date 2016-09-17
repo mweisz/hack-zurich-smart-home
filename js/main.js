06336d796eec42653890f1236efd85fe3d074a15
Michael Weisz <mail@michael-weisz.de>
2016-09-17 04:26:14 +0200

    Add manual toggle mechanism.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
 // GLOBALS, Yummy!!!
 var minute = 0; 
-var sampleData = [[0, 0, 0, 1, 1, 1],
-                  [1, 0, 1, 0, 1, 0]]; 
+var historicData = [stateTimeline.contact.contact_3,
+                    stateTimeline.contact.contact_13]; 
 var simulationInterval;
 var isSimulationRunning = false;
 
 // CONSTANTS
-var PERIOD_DURATION = 1000; // 1 sec
+var PERIOD_DURATION = 1; // 1 sec
 
 $(function(){
     $('.lightbulb').click(function(target){
+        var lightIndex = parseInt(target.toElement.id.split("\-")[1]);
+        var lightbulb = $(target.toElement);
+        if (lightbulb.hasClass('lightbulb-on')) {
+            turnOffLight(lightIndex);
+        } else {
+            turnOnLight(lightIndex);
+        }
+    });
+
+    $('.overlay-box').click(function(target){
+        var doorIndex = parseInt(target.toElement.id.split("\-")[1]);
+
+        if($(target.toElement).hasClass('vertical-door-closed') || $(target.toElement).hasClass('horizontal-door-closed')){
+            openDoor(doorIndex);
+        } else {
+            closeDoor(doorIndex);
+        }
 
     });
 });
@@ -25,18 +42,18 @@ function toggleSimulation() {
 function startSimulation() {
     isSimulationRunning = true;
     simulationInterval = setInterval(function() {
-        console.log('Minute', minute);
+       // console.log('Minute', minute);
 
         updateDoors();
-        minute = (minute + 1) % sampleData[0].length;
+        minute = (minute + 1) % historicData[0].length;
     }, PERIOD_DURATION);
 }
 
 function updateDoors() {
-    for (var doorIndex = 0; doorIndex < sampleData.length; doorIndex++) {
-        if (sampleData[doorIndex][minute] == 0 && (minute == 0 || sampleData[doorIndex][minute - 1] != 0)) {
+    for (var doorIndex = 0; doorIndex < historicData.length; doorIndex++) {
+        if (historicData[doorIndex][minute] == 0 && (minute == 0 || historicData[doorIndex][minute - 1] != 0)) {
                 openDoor(doorIndex + 1);
-        } else if (sampleData[doorIndex][minute] == 1 && (minute == 0 || sampleData[doorIndex][minute - 1] != 1)) {
+        } else if (historicData[doorIndex][minute] == 1 && (minute == 0 || historicData[doorIndex][minute - 1] != 1)) {
                 closeDoor(doorIndex + 1);
         }        
     }
@@ -63,6 +80,7 @@ function turnOffLight(lightIndex) {
 }
 
 function openDoor(doorIndex) {
+    console.log('Open doors.');
     var door = $('#door-' + doorIndex);
     if (door.hasClass('vertical-door-closed')) {
         // Vertical door
@@ -74,6 +92,8 @@ function openDoor(doorIndex) {
 }
 
 function closeDoor(doorIndex) {
+    console.log('Close doors.');
+
     var door = $('#door-' + doorIndex);
     if (door.hasClass('vertical-door-open')) { 
         // Vertical door
